997106c93bf04ad97525393210ba6c1b98f308a2
PalomoGonzalo <89616271+PalomoGonzalo@users.noreply.github.com>
2022-08-25 09:02:32 -0300

    Add files via upload

---
+++ Clase01/ap09.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Realizar las líneas de código necesarias para generar un Array asociativo $lapicera, que
+contenga como elementos: ‘color’, ‘marca’, ‘trazo’ y ‘precio’. Crear, cargar y mostrar tres
+lapiceras.
+
+
+
+*/
+$lapiceras=array(array("Color"=>"rojo","trazo"=>"fino","precio"=>25,"marca"=>"bic"),array("Color"=>"azul","trazo"=>"fino","precio"=>25,"marca"=>"bic"),array("Color"=>"blanco","trazo"=>"fino","precio"=>55,"marca"=>"bic"));
+
+var_dump($lapiceras);
+
+
+
+?>
